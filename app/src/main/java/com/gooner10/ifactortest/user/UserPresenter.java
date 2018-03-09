36f8f5889a53792dda21917f48b3da2c498c02fa
Birat Rai <biratrai@hotmail.com>
2018-03-08 22:03:35 -0500

    using new retrofit2api

--- app/src/main/java/com/gooner10/ifactortest/user/UserPresenter.java
+++ app/src/main/java/com/gooner10/ifactortest/user/UserPresenter.java
@@ -8,10 +8,10 @@ import com.gooner10.ifactortest.network.ServiceGenerator;
 
 import java.util.List;
 
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Response;
-import retrofit.Retrofit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 /**
  * Listens to user actions from the UI ({@link UserActivity}), retrieves the data and updates the
@@ -36,8 +36,8 @@ public class UserPresenter implements UserContract.UserPresenter {
         Call<List<Users>> call = service.getUserData();
         call.enqueue(new Callback<List<Users>>() {
             @Override
-            public void onResponse(Response<List<Users>> response, Retrofit retrofit) {
-                if (response.isSuccess()) {
+            public void onResponse(Call<List<Users>> call, Response<List<Users>> response) {
+                if (response.isSuccessful()) {
                     usersList = response.body();
                     mUserView.displayUserData(usersList);
                     Log.d(LOG_TAG, "Retrofit onResponse: " + usersList);
@@ -45,7 +45,7 @@ public class UserPresenter implements UserContract.UserPresenter {
             }
 
             @Override
-            public void onFailure(Throwable t) {
+            public void onFailure(Call<List<Users>> call, Throwable t) {
                 Log.d(LOG_TAG, "Retrofit onFailure: " + t.getMessage());
             }
         });
