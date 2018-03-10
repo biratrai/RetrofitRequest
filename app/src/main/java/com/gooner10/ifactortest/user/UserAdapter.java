cfcc715d19f43c0a5fe5f67be251b1cd1a60b434
Birat Rai <biratrai@hotmail.com>
2018-03-09 23:08:00 -0500

    formatting and adding @NonNull annotation to param, and removing unnecessary casting of view

--- app/src/main/java/com/gooner10/ifactortest/user/UserAdapter.java
+++ app/src/main/java/com/gooner10/ifactortest/user/UserAdapter.java
@@ -2,16 +2,17 @@ package com.gooner10.ifactortest.user;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.gooner10.ifactortest.detailpost.DetailActivity;
 import com.gooner10.ifactortest.R;
-import com.gooner10.ifactortest.util.Util;
+import com.gooner10.ifactortest.detailpost.DetailActivity;
 import com.gooner10.ifactortest.model.Users;
+import com.gooner10.ifactortest.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,14 +30,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).
                 inflate(R.layout.single_row_user, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.userNameText.setText(usersList.get(position).getName());
         holder.addressText.setText(Util.getAddress(usersList.get(position).getAddress()));
     }
@@ -58,8 +59,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         public ViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
-            userNameText = (TextView) itemView.findViewById(R.id.userName);
-            addressText = (TextView) itemView.findViewById(R.id.address);
+            userNameText = itemView.findViewById(R.id.userName);
+            addressText = itemView.findViewById(R.id.address);
         }
 
         @Override
