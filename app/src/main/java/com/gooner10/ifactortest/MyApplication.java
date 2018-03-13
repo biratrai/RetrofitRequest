def7af94a7c840e8fcfdcdbf7fa2ba010ab2152c
Birat Rai <biratrai@hotmail.com>
2018-03-12 22:56:44 -0400

    adding a method to return instance of the applicaiton

--- app/src/main/java/com/gooner10/ifactortest/MyApplication.java
+++ app/src/main/java/com/gooner10/ifactortest/MyApplication.java
@@ -7,8 +7,15 @@ import android.app.Application;
  */
 
 public class MyApplication extends Application {
+    private static MyApplication instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        instance = this;
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
     }
 }
