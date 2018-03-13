c70d5000ed3e128a9f460ea950cc0abc5805539c
Birat Rai <biratrai@hotmail.com>
2018-03-12 22:53:13 -0400

    adding base android application class

---
+++ app/src/main/java/com/gooner10/ifactortest/MyApplication.java
@@ -0,0 +1,14 @@
+package com.gooner10.ifactortest;
+
+import android.app.Application;
+
+/**
+ * Main Application class
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
