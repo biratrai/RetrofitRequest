d6654245ba1825a25a470330e10de7119b18f2b0
Birat Rai <biratrai@hotmail.com>
2018-03-14 21:52:25 -0400

    adding test for displaying UserData when response is successful

--- app/src/test/java/com/gooner10/ifactortest/user/UserPresenterTest.java
+++ app/src/test/java/com/gooner10/ifactortest/user/UserPresenterTest.java
@@ -4,13 +4,21 @@ import com.gooner10.ifactortest.model.Users;
 import com.gooner10.ifactortest.network.ApiService;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.Collections;
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 /**
@@ -23,14 +31,32 @@ public class UserPresenterTest {
     @Mock
     ApiService apiService;
 
+    @Mock
+    Call<List<Users>> mockCall;
+
     @Captor
     ArgumentCaptor<Callback<List<Users>>> argumentCaptor;
 
     private UserPresenter presenter;
+    private List<Users> usersList;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         presenter = new UserPresenter(view);
+        usersList = Collections.singletonList(new Users());
     }
 
+    @Test
+    public void loadUserData_displaysUserData_whenResponseIsSuccessful() throws Exception {
+        when(apiService.getUserData()).thenReturn(mockCall);
+        Response<List<Users>> listResponse = Response.success(usersList);
+
+        presenter.loadUserData(apiService);
+
+        verify(mockCall).enqueue(argumentCaptor.capture());
+        argumentCaptor.getValue().onResponse(null, listResponse);
+
+        verify(view).displayUserData(usersList);
+    }
 }
