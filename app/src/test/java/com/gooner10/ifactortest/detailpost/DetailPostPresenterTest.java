993b9b4d95d9fa8b76f0b793663ad8a8f01e5e05
Birat Rai <biratrai@hotmail.com>
2018-03-15 23:32:50 -0400

    adding test for detailPresenter

---
+++ app/src/test/java/com/gooner10/ifactortest/detailpost/DetailPostPresenterTest.java
@@ -0,0 +1,15 @@
+package com.gooner10.ifactortest.detailpost;
+
+import org.junit.Before;
+
+/**
+ * Test clas for {@link DetailPostPresenter}
+ */
+public class DetailPostPresenterTest {
+    private DetailPostPresenter postPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
