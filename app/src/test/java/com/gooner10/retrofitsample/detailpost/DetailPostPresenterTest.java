275c6c7277fcb59c2604a089c8dcb95f3183335e
Birat Rai <biratrai@hotmail.com>
2018-03-18 10:49:14 -0400

    arranging test for detailpostpresenter

--- app/src/test/java/com/gooner10/retrofitsample/detailpost/DetailPostPresenterTest.java
+++ app/src/test/java/com/gooner10/retrofitsample/detailpost/DetailPostPresenterTest.java
@@ -1,25 +1,29 @@
 package com.gooner10.retrofitsample.detailpost;
 
+import com.gooner10.retrofitsample.model.Posts;
 import com.gooner10.retrofitsample.network.ApiService;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.Map;
+import java.util.List;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okio.BufferedSource;
-import okio.Okio;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Test clas for {@link DetailPostPresenter}
@@ -27,39 +31,40 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @RunWith(JUnit4.class)
 public class DetailPostPresenterTest {
     private DetailPostPresenter postPresenter;
-    private MockWebServer mockWebServer;
-    private ApiService service;
+    private List<Posts> posts;
+
+    @Mock
+    DetailPostContract.View postView;
+
+    @Mock
+    ApiService service;
+
+    @Mock
+    Call<List<Posts>> mockCall;
+    @Captor
+    ArgumentCaptor<Callback<List<Posts>>> argumentCaptor;
 
     @Before
     public void setUp() throws IOException {
-        mockWebServer = new MockWebServer();
-        service = new Retrofit.Builder()
-                .baseUrl(mockWebServer.url("/"))
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(ApiService.class);
-    }
-
-    @After
-    public void tearDown() throws IOException {
-        mockWebServer.shutdown();
+        MockitoAnnotations.initMocks(this);
+        postPresenter = new DetailPostPresenter(postView, service);
+        posts = Collections.singletonList(new Posts("1",
+                "ola",
+                "body",
+                "12"));
     }
 
     @Test
-    public void loadPostsData_displaysPostData_whenSuccessful() throws InterruptedException, IOException {
-        enqueueResponse("user-detail.json");
+    public void loadPostsData_displaysPostData_whenResposeIsSuccessful() throws InterruptedException, IOException {
+        when(service.getUserPosts("1")).thenReturn(mockCall);
+        Response<List<Posts>> response = Response.success(posts);
+
+        postPresenter.loadPostsData("1");
+
+        verify(mockCall).enqueue(argumentCaptor.capture());
+        argumentCaptor.getValue().onResponse(mockCall, response);
+
+        verify(postView, times(1)).displayPostData(ArgumentMatchers.<Posts>anyList());
     }
 
-    private void enqueueResponse(String fileName) throws IOException {
-        enqueueResponse(fileName, Collections.<String, String>emptyMap());
-    }
-
-    private void enqueueResponse(String fileName, Map<String, String> headers) throws IOException {
-        InputStream inputStream = getClass().getClassLoader()
-                .getResourceAsStream("api-response/" + fileName);
-        BufferedSource source = Okio.buffer(Okio.source(inputStream));
-        MockResponse mockResponse = new MockResponse();
-        mockWebServer.enqueue(mockResponse
-                .setBody(source.readString(StandardCharsets.UTF_8)));
-    }
 }
