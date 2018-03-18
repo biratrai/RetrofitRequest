12fe5397289d41623bd80f109d401827c6eba7f3
Birat Rai <biratrai@hotmail.com>
2018-03-17 20:01:52 -0400

    changing the package name for application class

---
+++ app/src/main/java/com/gooner10/retrofitsample/MyApplication.java
@@ -0,0 +1,34 @@
+package com.gooner10.retrofitsample;
+
+import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Main Application class
+ */
+
+public class MyApplication extends Application {
+    private static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
+    }
+
+    public static boolean hasNetwork() {
+        return instance.checkIfHasNetwork();
+    }
+
+    private boolean checkIfHasNetwork() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
