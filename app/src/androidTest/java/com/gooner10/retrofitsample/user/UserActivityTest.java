922e3fd5db924f031e5a1940c7d2b5bdb78a3882
Birat Rai <biratrai@hotmail.com>
2019-04-16 22:34:03 -0400

    adding test for timeOut

--- app/src/androidTest/java/com/gooner10/retrofitsample/user/UserActivityTest.java
+++ app/src/androidTest/java/com/gooner10/retrofitsample/user/UserActivityTest.java
@@ -3,23 +3,41 @@ package com.gooner10.retrofitsample.user;
 import android.support.test.rule.ActivityTestRule;
 
 import com.gooner10.retrofitsample.MockWebServerRule;
-import com.jakewharton.espresso.OkHttp3IdlingResource;
+import com.gooner10.retrofitsample.R;
 
 import org.junit.Rule;
+import org.junit.Test;
 
-import okhttp3.OkHttpClient;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.mockwebserver.MockResponse;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class UserActivityTest {
 
     @Rule
-    ActivityTestRule<UserActivity> activityTestRule = new ActivityTestRule<>(UserActivity.class
+    public ActivityTestRule<UserActivity> activityTestRule = new ActivityTestRule<>(UserActivity.class
             , true, false);
 
     @Rule
-    MockWebServerRule mockWebServerRule = new MockWebServerRule();
+    public MockWebServerRule mockWebServerRule = new MockWebServerRule();
 
     @Rule
     OkHttp3IdlingResource okHttp3IdlingResource = OkHttp3IdlingResource.create("OkHttp", new OkHttpClient());
 
+    @Test
+    public void timeOut() {
+        mockWebServerRule.server.enqueue(new MockResponse().setBody("")
+                .throttleBody(1, 1, TimeUnit.SECONDS));
 
+        activityTestRule.launchActivity(null);
+
+
+        onView(withId(R.id.error_text))
+                .check(matches(withText("No Data available.")));
+    }
 }
